4551f3bc31b4a6181f7cb574af8664e9e4844272
9ziggy9 <davidarogers@protonmail.com>
2022-08-18 08:58:26 -0700

    magic foreach construct

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "./include/globals.h"
 
-void feed_forward() {
-  printf("%d %d", HEIGHT, WIDTH);
+#define foreach(item, array) \
+  for(int keep = 1, \
+	count = 0, \
+	size = sizeof (array) / sizeof *(array); \
+	keep && count != size; \
+	keep = !keep, count++) \
+	for(item = (array) + count; keep; keep = !keep)
+
+void f() {
+  int test[] = {1,2,3,4,5};
+  foreach(int *v, test) {
+    printf("value: %d\n", *v);
+  }
 }
 
 int main(void) {
