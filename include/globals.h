85d70837155da42a583c53866691226cd1e4f9ec
9ziggy9 <davidarogers@protonmail.com>
2022-08-18 10:32:45 -0700

    rect struct and layer filling

--- include/globals.h
+++ include/globals.h
@@ -1,2 +1,7 @@
+#ifndef CARDS_H_
+#define CARDS_H_
+
 #define WIDTH 20
 #define HEIGHT 20
+
+#endif
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include "./include/globals.h"
 
 // Be very suspicious of any macro that attempts to extend language syntax;
@@ -10,15 +11,52 @@
 	keep = !keep, count++) \
 	for(item = (array) + count; keep; keep = !keep)
 
-void f() {
-  int test[] = {1,2,3,4,5};
-  foreach(int *v, test) {
-    printf("value: %d\n", *v);
+typedef float Layer[HEIGHT][WIDTH];
+typedef struct Rect {
+  int x; int y;
+  int w; int h;
+} Rect;
+
+// Assembly generated from this implementation is praticularly good.
+int clamp(int d, int min, int max) {
+  const int t = d < min ? min : d;
+  return t > max ? max : t;
+}
+
+void layer_fill_rect(Layer layer, Rect rect, float v) {
+  assert(rect.w > 0);
+  assert(rect.h > 0);
+  int x0 = clamp(rect.x, 0, WIDTH-1);
+  int y0 = clamp(rect.y, 0, HEIGHT-1);
+  int x1 = clamp(x0 + rect.w - 1, 0, WIDTH-1);
+  int y1 = clamp(y0 + rect.h - 1, 0, HEIGHT-1);
+  for (int y = y0; y <= y1; y++) {
+    for (int x = x0; x <= x1; x++) {
+      layer[y][x] = v;
+    }
   }
 }
 
+//                     data           model
+float feed_forward(Layer input, Layer weights) {
+  float output = 0.0f;
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      output += input[y][x] * weights[y][x];
+    }
+  }
+  return output;
+}
+
+static Layer input;
+static Layer weights;
+
 int main(void) {
-  printf("Hello, world\n");
-  f();
+  float output = feed_forward(input, weights);
+
+  Rect rect = {0,0,WIDTH/2,HEIGHT/2};
+  layer_fill_rect(input, rect, 1.0f);
+
+  printf("output = %f\n", output);
   return 0;
 }
